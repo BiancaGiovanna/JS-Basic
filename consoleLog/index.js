07e4bf9a0483a2ac37ee39b1944861ca09cf7f02
BiancaGiovanna <biancagothicg@gmail.com>
2021-11-24 06:07:11 -0300

    console log

---
+++ consoleLog/index.js
@@ -0,0 +1,14 @@
+/* String's, JS é uma linguagem com tipagem dinamica e fraca então não é 
+*  necessario passar um tipo para as declarações
+*/
+
+console.log('Bianca Gomes');
+console.log("Bianca Gomes");
+console.log(`Giovanna`)
+
+/*
+  Literais 
+  Number, como não existe tipagem forte não exite float, int ou double tudos são
+  declarados como Number
+*/
+console.log(1234, 15.85);
