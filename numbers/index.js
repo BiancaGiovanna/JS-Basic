2d968f9feb00b6940ea8e0500276ddadd6b0f779
BiancaGiovanna <biancagothicg@gmail.com>
2021-12-29 00:23:02 -0300

    numbers

---
+++ numbers/index.js
@@ -0,0 +1,12 @@
+let num1 = 20.451;
+let num2 = 10;
+
+let temp = num1 * 'ola';
+console.log(Number.isNaN(temp));
+/**
+ * Metodos number
+ * toString
+ * toFixed
+ * Number.isInteger(num2)
+ * isNaN
+ */
