9a0901ac07cb517970cf4d60889a1d0e3ef97627
BiancaGiovanna <biancagothicg@gmail.com>
2022-01-06 09:52:09 -0300

    objeto math

---
+++ objetcMath/index.js
@@ -0,0 +1,10 @@
+let num1 = 16;
+
+// let num2 = Math.floor(num1)  // arrendondar pra baixo
+// let num2 = Math.ceil(num1)  // arrendondar pra cima
+// let num2 = Math.round(); //arredondamento automatico
+// console.log(Math.max(1, 2, 3, 4, 5, -10, -50, 1500, 9, 8, 7, 6)) //pega o maior numero de uma sequencia
+// console.log(Math.min(1, 2, 3, 4, 5, -10, -50, 1500, 9, 8, 7, 6)) //pega o menor numero de uma sequencia
+
+// const aleatorio = Math.random();
+
