66cb6621f2481c74f83b1e4a231592297df764fb
BiancaGiovanna <biancagothicg@gmail.com>
2022-01-27 09:42:08 -0300

    funcoes basico

---
+++ funcoesBasico/index.js
@@ -0,0 +1,16 @@
+// function soma(x, y) {
+//   const resultado = x + y;
+//   return resultado;
+// }
+
+// console.log(soma(10, 20))
+
+// const raiz = function(n) {
+//   return n ** 0.5;
+// };
+
+// console.log(raiz(9));
+
+const raiz = n => n ** 0.5
+
+console.log(raiz(9));
